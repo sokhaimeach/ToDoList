4c432cc81af5b20079a82a4d7a7793dfbb554db1
Meach Sokhai <dshhshh750@gmail.com>
2025-06-13 10:15:56 +0700

    make 2 fn more

--- todoscript.js
+++ todoscript.js
@@ -25,15 +25,46 @@ function showList() {
     allList.innerHTML = "";
     let iconId = 'index';
     for(let i = 0; i < arrList.length; i++){
-        iconId = 'index' + i;
+        // iconId = 'index' + i;
         allList.insertAdjacentHTML('beforeend', `
             <div class="list">
-                <i class="bi bi-circle check-list" class="${iconId}"></i>
+                <i class="check-list bi bi-circle"></i>
                 <p class="text">${arrList[i]}</p>
-                <i class="bi bi-trash trash-list" class="${iconId}"></i>
+                <i class="bi bi-trash trash-list"></i>
             </div>
         `);
     }
+    const checked = document.querySelectorAll('.check-list');
+    const trash = document.querySelectorAll('.trash-list');
+    const listText = document.querySelectorAll('.text');
+    checkedList(checked);
+    removeList(trash, listText);
+}
+
+function checkedList(checks) {
+    checks.forEach(check => {
+        check.addEventListener('click', () => {
+            if(check.className == 'check-list bi bi-circle'){
+                check.classList.remove('bi-circle');
+                check.classList.add('bi-check-circle-fill');
+                check.style.color = 'var(--accent-color)';
+            } else {
+                check.classList.remove('bi-check-circle-fill');
+                check.classList.add('bi-circle');
+                check.style.color = 'var(--text-color)';
+            }
+        })
+    })
+}
+
+function removeList(trash, listText){   
+    trash.forEach((t, i) => {
+        t.addEventListener('click', () => {
+            const id = listText[i].innerText;
+            arrList = arrList.filter(list => list !== id);
+            showList();
+        });
+    });
 }
 
 
